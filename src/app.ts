7eb49119b9553f8afe3600a044fab20f9558d420
Rasolomampionona Henintsoa Heriniaina <rasolomampiononahenintsoaherin@gmail.com>
2026-01-29 18:22:52 +0100

    Simplify CORS configuration to allow all origins
    Removed specific CORS origin restrictions and enabled CORS for all origins.

--- src/app.ts
+++ src/app.ts
@@ -25,26 +25,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(logMiddleware);
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://power-tasks.vercel.app",
-];
+
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  cors()
 );
 
 job.start();
